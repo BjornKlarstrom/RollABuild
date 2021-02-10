a17e22872a5648a5090bc91f613966acc3a9e81a
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2021-02-10 20:51:40 +0100

    adds small fix to joystick scene

---
+++ Assets/Scripts/JoystickMoveController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickMoveController : MonoBehaviour
+{
+    public float speed;
+    public Joystick joystick;
+    public Rigidbody rb;
+    public void FixedUpdate()
+    {
+        var direction = Vector3.forward * this.joystick.Vertical + Vector3.right * this.joystick.Horizontal;
+        this.rb.AddForce(direction * (speed * Time.fixedDeltaTime), ForceMode.VelocityChange);
+    }
+}
